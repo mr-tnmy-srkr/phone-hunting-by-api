c564a7c01ec4f833e9fff632fd40278fd1604030
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-29 02:22:16 +0530

    MODAL UI ADDED

--- phone.js
+++ phone.js
@@ -41,8 +41,8 @@ if(!isShowAll){
    <div class="card-body">
        <h2 class="card-title">${phone.phone_name}</h2>
        <p>If a dog chews shoes whose shoes does he choose?</p>
-       <div class="card-actions justify-end">
-           <button class="btn btn-primary">Buy Now</button>
+       <div class="card-actions justify-center">
+           <button onclick="handelShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
        </div>
    </div>
    `;
@@ -76,4 +76,13 @@ if(isLoading){
 // handel show all
 const handelShowAll = () =>{
   handelSearch(true)
+}
+
+// modal
+const handelShowDetails = async(id) =>{
+  // console.log(id)
+  // load single phone data
+const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+const data = await res.json();
+console.log(data)
 }
