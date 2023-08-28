4fa324fb2a5adc4e99facc8ce261278c09e8f29a
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-28 23:54:40 +0530

    updated

--- phone.js
+++ phone.js
@@ -3,7 +3,32 @@ const loadPhone = async () => {
     "https://openapi.programming-hero.com/api/phones?search=iphone"
   );
   const data = await res.json();
+  // console.log(data.data)
   const phones = data.data;
-console.log(phones)
+  displayPhones(phones);
+  // console.log(phones);
 };
+
+const displayPhones = (phones) => {
+  // console.log(phones);
+  const phoneContainer = document.getElementById("phone-container");
+
+  phones.forEach(phone => {
+    console.log(phone);
+   const phoneCard = document.createElement('div');
+   phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`
+   phoneCard.innerHTML =
+   ` <figure><img src="${phone.image}" alt="Shoes" /></figure>
+   <div class="card-body">
+       <h2 class="card-title">Shoes!</h2>
+       <p>If a dog chews shoes whose shoes does he choose?</p>
+       <div class="card-actions justify-end">
+           <button class="btn btn-primary">Buy Now</button>
+       </div>
+   </div>
+   `;
+   phoneContainer.appendChild(phoneCard);
+  });
+};
+
 loadPhone();
