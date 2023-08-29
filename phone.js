8bd71c6e2111030ca7273f07489e394c210e96b2
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-29 13:28:45 +0530

    almost complete

--- phone.js
+++ phone.js
@@ -53,7 +53,7 @@ const displayPhones = (phones, isShowAll) => {
     phoneCard.innerHTML = ` <figure><img src="${phone.image}" alt="Shoes" /></figure>
    <div class="card-body">
        <h2 class="card-title">${phone.phone_name}</h2>
-       <p>If a dog chews shoes whose shoes does he choose?</p>
+       <p>There are many variations of passages of available, but the majority have suffered</p>
        <div class="card-actions justify-center">
            <button onclick="handelShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
        </div>
@@ -107,17 +107,26 @@ const showPhoneDetails = (phone) => {
   console.log(phone);
   const phoneName = document.getElementById("show-detail-phone-name");
   phoneName.innerText = phone.name;
+  phoneName.classList.add("text-center")
   console.log(phone.name);
   const showDetailsContainer = document.getElementById("show-detail-container");
   showDetailsContainer.innerHTML = `
-<img src ="${phone.image}" alt ="" />
-<p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
-<p><span>GPS:</span>${phone.others?.GPS || "No GPS available"}</p>
-<p><span>GPS:</span>${
+<div class ="flex justify-center my-5"><img src ="${phone.image}" alt ="" /></div>
+<div>
+<p><span class="font-bold">Brand : </span>${phone?.brand}</p>
+<p><span class="font-bold">Storage : </span>${phone?.mainFeatures?.storage}</p>
+<p><span class="font-bold">Display Size : </span>${phone?.mainFeatures?.displaySize}</p>
+<p><span class="font-bold">Memory : </span>${phone?.mainFeatures?.memory}</p>
+<p><span class="font-bold">Chipset : </span>${phone?.mainFeatures?.chipSet}</p>
+<p><span class="font-bold">Release Date : </span>${phone?.releaseDate}</p>
+
+<p><span class="font-bold">GPS:</span>${phone.others?.GPS || "No GPS available"}</p>
+<p><span class="font-bold">GPS : </span>${
     phone.others?.GPS ? phone.others.GPS : "No GPS available in this device"
   }</p>
+</div>
 `;
-
+showDetailsContainer.classList = ``
   show_modal.showModal();
 };
 
